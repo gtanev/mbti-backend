c97c9a3ea214610b86b82222008f66cb93e5c16b
George Tanev <georgetjazz@gmail.com>
2019-07-12 20:48:04 +0200

    Include SQL logging flag in config.json.

--- persistence/db.js
+++ persistence/db.js
@@ -2,7 +2,7 @@ import { Sequelize } from 'sequelize';
 import config from '../config';
 import fs from 'fs';
 
-const { host, database, dialect, username, password } = config.development;
+const { host, database, dialect, username, password, logging } = config.development;
 
 export const initDB = async () => {
   await new Sequelize('', username, password, { host: host, dialect: dialect })
@@ -11,12 +11,13 @@ export const initDB = async () => {
 
 export const fillDB = () => {
   fs.readFile('./resources/init-data.sql', 'utf8', async (err, contents) => {
-    await sequelise.query(contents, { logging: false });
+    await sequelise.query(contents, { logging: logging });
   });
 };
 
 export const sequelise = new Sequelize(database, username, password, {
   host: host,
   dialect: dialect,
+  logging: logging,
   dialectOptions: { multipleStatements: true }
 });
